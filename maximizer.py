e0ac7fd3cc3f2d648cbf503ecbbeaf9aaeac1397
Dhruvaa Saravanan <dhruvaa@pop-os.localdomain>
2022-02-03 03:13:55 -0500

    Finished inital MVP brute force approach

--- maximizer.py
+++ maximizer.py
@@ -2,11 +2,21 @@ from rules import RuleSet
 from itertools import permutations
 
 def reduced_bf(spendings: dict) -> int:
-    points = 0
     valid_rules = get_valid_rules(spendings)
     permutes = list(permutations(valid_rules))
-    print(permutes)
-    return points
+    points_from_permutes = []
+    for permute in permutes:
+        points = 0
+        spendings_to_try = spendings.copy()
+        for i in permute:
+            rule = RuleSet().rules[i]
+            times_to_app = min([spendings_to_try[debit] // rule[1][debit] for debit in rule[1].keys()])
+            applied = RuleSet().apply_rule(spendings_to_try, i, times_to_app)
+            points += applied[0]
+            spendings_to_try = applied[1]
+        points += RuleSet().apply_rule(spendings_to_try, 0)[0]
+        points_from_permutes.append(points)
+    return max(points_from_permutes)
 
 def get_valid_rules(spendings: dict) -> list:
     total_rules = RuleSet().rules
@@ -22,7 +32,7 @@ def get_valid_rules(spendings: dict) -> list:
             valid_rules.append(i)
     return valid_rules
 
-print(reduced_bf({"sport_chek": 75, "subway": 25}))
+print(reduced_bf({"sport_chek": 25, "tim_hortons": 10, "the_bay": 5}))
 
 # rules = [1, 2, 3, 4, 5]
 # print(list(permutations(rules)))
