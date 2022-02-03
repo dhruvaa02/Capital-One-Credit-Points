937faf98ab322b5452282a63ab26adfb0015f6e1
Dhruvaa Saravanan <dhruvaa@pop-os.localdomain>
2022-02-02 22:21:57 -0500

    Testing permutations

---
+++ maximizer.py
@@ -0,0 +1,8 @@
+import rules
+from itertools import permutations
+
+def reduced_bf(spendings: dict) -> int:
+    points = 0
+    return points
+rules = [1, 2, 3, 4, 5]
+print(list(permutations(rules)))
