f6cb7ef75125319b7711080ae446ad3d76f4c743
Dhruvaa Saravanan <dhruvaa@pop-os.localdomain>
2022-02-04 17:47:12 -0500

    Dynamically creating the rules for 3 explcit companies

--- dynamic_lpp.py
+++ dynamic_lpp.py
@@ -10,14 +10,10 @@ rules = [
         (75, {"sport_chek": 20})
     ]
 
+monthly = {"sport_chek": 25, "tim_hortons": 10, "subway": 0, "the_bay": 5}
+
 solver = pywraplp.Solver.CreateSolver('SCIP')
 
-# x1 = solver.IntVar(0, solver.infinity(), 'x1')
-# x2 = solver.IntVar(0, solver.infinity(), 'x2')
-# x3 = solver.IntVar(0, solver.infinity(), 'x3')
-# x4 = solver.IntVar(0, solver.infinity(), 'x4')
-# x5 = solver.IntVar(0, solver.infinity(), 'x5')
-# x6 = solver.IntVar(0, solver.infinity(), 'x6')
 x = [0]
 
 sports_expr = ""
@@ -29,35 +25,38 @@ for i in range(1, len(rules)):
     if "sport_chek" in rules[i][1]:
         if i == 1:
             sports_expr = sports_expr + f"{rules[i][1]['sport_chek']}*x[{i}]"
+        else:
+            sports_expr = sports_expr + f" + {rules[i][1]['sport_chek']}*x[{i}]"
+    if "tim_hortons" in rules[i][1]:
+        if i == 1:
+            tims_expr = tims_expr + f"{rules[i][1]['tim_hortons']}*x[{i}]"
+        else:
+            tims_expr = tims_expr + f" + {rules[i][1]['tim_hortons']}*x[{i}]"
+    if "subway" in rules[i][1]:
+        if i == 1:
+            sub_expr = sub_expr + f"{rules[i][1]['subway']}*x[{i}]"
+        else:
+            sub_expr = sub_expr + f" + {rules[i][1]['subway']}*x[{i}]"
 
-print(sports_expr)
-print(tims_expr)
-print(sub_expr)
-sport = 2000
-tims = 1469
-sub = 890
-other = 7328
+sports_expr = sports_expr + f" <= {monthly['sport_chek']}"
+tims_expr = tims_expr + f" <= {monthly['tim_hortons']}"
+sub_expr = sub_expr + f" <= {monthly['subway']}"
 
-solver.Add(eval("75*x[1] + 75*x[2] + 75*x[3] + 25*x[4] + 25*x[5] + 20*x[6] <= sport"))
-solver.Add(eval("25*x[1] + 25*x[2] + 10*x[4] + 10*x[5] <= tims"))
-solver.Add(eval("25*x[1] + 10*x[4] <= sub"))
+other = 0
+for shop in monthly.keys():
+    if shop not in "sport_chek tim_hortons subway":
+        other += monthly[shop]
+
+solver.Add(eval(sports_expr))
+solver.Add(eval(tims_expr))
+solver.Add(eval(sub_expr))
 
 solver.Maximize(eval("x[1]*500 + x[2]*300 + x[3]*200 + x[4]*150 + x[5]*75 + x[6]*75"))
 
 status = solver.Solve()
 
-x[0] = sum([sport - (75*x[1].solution_value() + 75*x[2].solution_value() + 75*x[3].solution_value() + 25*x[4].solution_value() + 25*x[5].solution_value() + 20*x[6].solution_value()), tims - (25*x[1].solution_value() + 25*x[2].solution_value() + 10*x[4].solution_value() + 10*x[5].solution_value()), sub - (25*x[1].solution_value() + 10*x[4].solution_value()), other])
+x[0] = sum([monthly['sport_chek'] - (75*x[1].solution_value() + 75*x[2].solution_value() + 75*x[3].solution_value() + 25*x[4].solution_value() + 25*x[5].solution_value() + 20*x[6].solution_value()), monthly['tim_hortons'] - (25*x[1].solution_value() + 25*x[2].solution_value() + 10*x[4].solution_value() + 10*x[5].solution_value()), monthly['subway'] - (25*x[1].solution_value() + 10*x[4].solution_value()), other])
 
-# if status == pywraplp.Solver.OPTIMAL:
-#     print('Solution:')
-#     print('Objective value =', solver.Objective().Value() + x7)
-#     print('x1 =', x1.solution_value())
-#     print('x2 =', x2.solution_value())
-#     print('x3 =', x3.solution_value())
-#     print('x4 =', x4.solution_value())
-#     print('x5 =', x5.solution_value())
-#     print('x6 =', x6.solution_value())
-#     print('x7 =', x7)
 if status == pywraplp.Solver.OPTIMAL:
     print('Solution:')
     print('Objective value =', solver.Objective().Value() + x[0])
